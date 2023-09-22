a99ec3a4dc896c13db2c758a90f377d5a535f546
«Matvey <mymayl@inbox.ru>
2023-09-22 20:00:02 +0500

    27

---
+++ Zad1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+void FillArray(int n)
+{
+    if(n > 0)
+    {
+        System.Console.Write($"{n} ");
+        FillArray(n -1);
+    }
+}
+
+ System.Console.WriteLine("Введите натуральное число "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+FillArray(n);
---
+++ Zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+string PrintNumbers(int m, int n) 
+{ 
+    if (n < m) 
+        return ""; 
+    return PrintNumbers(m, n - 1) + $"{n} "; 
+} 
+ 
+
+
+
+ 
+System.Console.WriteLine("Введите нижнюю границу M"); 
+int min = Convert.ToInt32(Console.ReadLine()); 
+System.Console.WriteLine("Введите верхнюю границу N"); 
+int max = Convert.ToInt32(Console.ReadLine()); 
+PrintNumbers(min, max);
