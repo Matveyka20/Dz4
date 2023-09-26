6388616f01aa6c87b79cdbdc372c8a0842424368
«Matvey <mymayl@inbox.ru>
2023-09-26 19:01:24 +0500

    29

--- Zad3/Program.cs
+++ Zad3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman(int M, int N)
+{
+    if (M ==0)
+    {
+        return N+1;
+    }
+    else if( M > 0 && N == 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return Akkerman(M-1, Akkerman(M, N-1));
+    }
+}
+
+
+
+Console.WriteLine("Введите число M"); 
+int M = Convert.ToInt32(Console.ReadLine()); 
+System.Console.WriteLine("Введите число N"); 
+int N = Convert.ToInt32(Console.ReadLine()); 
+System.Console.WriteLine(Akkerman(M,N));
+
